f68953cd0db79b020a3f5087a7f6ada5e24bcde2
jonatan.leanderz <jonatan.leanderz@KARR9027Y2M>
2017-01-13 11:29:02 +0100

    Serversaker för programmet

--- PongGongOnlineDeluxe/PongGongOnlineDeluxe/Network.cs
+++ PongGongOnlineDeluxe/PongGongOnlineDeluxe/Network.cs
@@ -2,10 +2,57 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using Lidgren.Network;
 
 namespace PongGongOnlineDeluxe
 {
+
+
+
+
     class Network
     {
+
+
+
+        NetPeerConfiguration netconfig;
+        
+        NetServer server;
+        NetClient client;
+
+        NetIncomingMessage msgRecieve;
+        NetOutgoingMessage msgSend;
+
+
+
+        public Network(string serverIdentifier)
+        {
+            // netpiercconfiguration argumentet är som en sorts identifier så att man vet att spelarna kopplar upp rätt. Båda klienterna måste använda samma identifier
+            
+
+            netconfig = new NetPeerConfiguration(serverIdentifier);
+            netconfig.Port = 1337;
+            netconfig.EnableMessageType(NetIncomingMessageType.Data);
+
+            server = new NetServer(netconfig);
+        }
+
+        void startServer()
+        {
+            server = new NetServer(netconfig);
+        }
+
+        void closeServer()
+        {
+           
+        }
+
+
+        bool send(string message)
+        {
+            return false;
+        }
+           
+
     }
 }
