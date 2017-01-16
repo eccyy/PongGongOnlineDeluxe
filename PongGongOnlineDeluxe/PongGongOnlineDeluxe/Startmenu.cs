8f430e16005af279c552cbb26574df6750658c57
jonatan.leanderz <jonatan.leanderz@KARR9027Y2M>
2017-01-16 14:09:33 +0100

    knappen koin gör nåt

--- PongGongOnlineDeluxe/PongGongOnlineDeluxe/Startmenu.cs
+++ PongGongOnlineDeluxe/PongGongOnlineDeluxe/Startmenu.cs
@@ -58,5 +58,10 @@ namespace PongGongOnlineDeluxe
             else return false;
             
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            join();
+        }
     }
 }
