6463965b747000f475145d39e5ab7c28707a1913
jonatan.leanderz <jonatan.leanderz@KARR9027Y2M>
2017-01-16 13:49:06 +0100

    Login och server ska fungera, hehe

--- PongGongOnlineDeluxe/PongGongOnlineDeluxe/Startmenu.cs
+++ PongGongOnlineDeluxe/PongGongOnlineDeluxe/Startmenu.cs
@@ -12,9 +12,51 @@ namespace PongGongOnlineDeluxe
 {
     public partial class Startmenu : Form
     {
+        Network network;
+
+
         public Startmenu()
         {
             InitializeComponent();
+        }
+
+        private void btnServerHost_Click(object sender, EventArgs e)
+        {
+            if(tbxServerName.Text.Length  > 0){
+
+                try
+                {
+                    network = new Network(tbxServerName.Text);
+                    network.startServer();
+                    network.connectServer("127.0.0.1");
+                    MessageBox.Show("success");
+                }
+                catch(Exception connectException)
+                {
+                    MessageBox.Show("an error occured: " + connectException.Message);
+                }
+            
+
+            }
+        }
+
+        
+        
+        private bool join()
+        {
+            if (tbxIpAdress.Text.Length > 6)
+            {
+                network = new Network(tbxServerName.Text);
+
+              if( network.connectServer(tbxIpAdress.Text.ToString()) == true)
+                {
+                    MessageBox.Show("success connected");
+                }
+
+                return true;
+            }
+            else return false;
+            
         }
     }
 }
