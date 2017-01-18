59d33825edc50cfe3e4f63bdc1c65c60cd00ae3c
jonatan.leanderz <jonatan.leanderz@KARR9027Y2M>
2017-01-18 10:10:21 +0100

    Klienten är typ klar,. Den behöver hämta meddelanden från servern. Inga kopplingar kan ske från internet till klienten.

--- PongGongOnlineDeluxe/PongGongOnlineDeluxe/Client.cs
+++ PongGongOnlineDeluxe/PongGongOnlineDeluxe/Client.cs
@@ -14,11 +14,11 @@ namespace PongGongOnlineDeluxe
 
 
         NetClient client;
-
-
         NetPeerConfiguration netconfig;
         NetIncomingMessage msgRecieve;
         NetOutgoingMessage msgSend;
+
+        NetConnection connection;
 
         string serverIp;
 
@@ -27,12 +27,58 @@ namespace PongGongOnlineDeluxe
             netconfig = new NetPeerConfiguration("abc");
             netconfig.Port = 1337;
             netconfig.EnableMessageType(NetIncomingMessageType.Data);
+            netconfig.EnableMessageType(NetIncomingMessageType.ConnectionApproval);
+            netconfig.EnableMessageType(NetIncomingMessageType.ConnectionLatencyUpdated);
 
+            client = new NetClient(netconfig);             
+                            
+        }
 
-            client = new NetClient(netconfig);
-        // lägga till exception hantering
+        public bool startClient()
+        {
             client.Start();
 
+            if (client.Status == NetPeerStatus.Running)            
+                return true;            
+            else
+                return false;
+
+        }
+
+        public bool connectToServer(string ip, string serverIdentifier)
+        {
+            try
+            {
+                                
+                client.Connect(host: serverIp, port: 1337);
+
+
+                while (client.ConnectionStatus != NetConnectionStatus.Connected)
+                {
+                    if (client.ConnectionsCount > 0)
+                    {
+                        return true;
+                    }
+                }
+                return false;
+
+            }
+            catch (Exception e)
+            {
+                return false;
+            }            
+        }
+
+        public bool sendMessage(string message)
+        {
+
+            var messageToSend = client.CreateMessage();
+            messageToSend.Write(message);
+
+            if (client.SendMessage(messageToSend, NetDeliveryMethod.ReliableOrdered) == NetSendResult.Sent)            
+                return true;
+            else                               
+                return false;
         }
 
 
